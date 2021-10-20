88563827222260e672cf5c89988711d312e6ad6b
Metral <e.b.metral@gmail.com>
2021-10-20 17:35:08 +0200

    IFDEF

--- include/dichotomy.h
+++ include/dichotomy.h
@@ -1,1 +1,6 @@
+#ifndef DICHOTOMY_H
+#define DICHOTOMY_H
+
 int find_by_dichotomy(int array[], int size_t, int value );
+
+#endif
--- include/knacksack.h
+++ include/knacksack.h
@@ -1,10 +1,14 @@
+#ifndef KNACKSACK_H
+#define KNACKSACK_H
+
 typedef struct 
 {
     int value;
     int weight;
 } Object;
 
-
 Object *knacksack(Object *tab, int size_t, int weightMax);
 void printObjectTab(Object *tab, int size_t);
+#endif 
+
 
